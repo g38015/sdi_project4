7abce1022fdc15676309efd618e1ac0e8fe3bfb8
appstrategy <eprleads@gmail.com>
2013-04-29 13:02:03 -0400

    updated function numbers and instructions for each function

--- rename-me.js
+++ rename-me.js
@@ -8,63 +8,90 @@
 // Library
 
 // String Functions 
+
+// Function 1
+// Does a string follow a 123-456-7890 pattern like a phone number?
 var stringPhone = function () {
 
 
 };
 
+// Function 2
+// Does a string follow an aaa@bbb.ccc pattern like an email address?
 var stringEmail = function () {
 
 
 };
 
+// Function 3
+// Is the string a URL? (Does it start with http: or https:?).
 var stringUrl = function () {
 
 
 };
 
+// Function 4
+// Title-case a string (split into words, then uppercase the first letter of each word).
 var stringTitle = function () {
 
 
 };
 
+// Function 5
+// Given a string that is a list of things separated by a given string, as well as another string separator, return a string with the first separator changed to the second: "a,b,c" + "," + "/" → "a/b/c".
 var stringSeparator = function () {
 
 
 };
 
 // Number Functions
+
+// Function 6
+// Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10.
 var numDecimal = function () {
 
 
 };
 
+// Function 7
+// Fuzzy-match a number: is the number above or below a number within a certain percent?
 var numFuzzy = function () {
 
 
 };
 
+// Function 8
+// Find the number of hours or days difference between two dates.
 var numDates = function () {
 
 
 };
 
+// Function 9
+// Given a string version of a number such as "42", return the value as an actual Number, such as 42.
 var numNumber = function () {
 
 
 };
 
 // Array Functions
+
+// Function 10
+// Find the smallest value in an array that is greater than a given number.
 var arrValue = function () {
 
 
 };
 
+// Function 11
+// Find the total value of just the numbers in an array, even if some of the items are not numbers.
 var arrTotal = function () {
 
 
 };
 
+// Function 12
+// Given an array of objects and the name of a key, return the array sorted by the value of that key in each of the objects: "a" + [{a:2},{a:3},{a:1}] → [{a:1},{a:2},{a:3}].
 var arrSorted = function () {
 
 
