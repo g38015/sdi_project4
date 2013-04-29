991d7652a6eba1b70ca82c882d969f4e31c657da
appstrategy <eprleads@gmail.com>
2013-04-29 12:48:02 -0400

    stubed all library functions

--- rename-me.js
+++ rename-me.js
@@ -1,6 +1,73 @@
 // alert("JavaScript works!");
+// Github https://github.com/g38015/sdi_project4
+// Peter Hitchcock
+// SDI 1304
+// April 29, 2013
+// Project 4
 
+// Library
+
+// String Functions 
 var stringPhone = function () {
 
 
-}
+};
+
+var stringEmail = function () {
+
+
+};
+
+var stringUrl = function () {
+
+
+};
+
+var stringTitle = function () {
+
+
+};
+
+var stringSeparator = function () {
+
+
+};
+
+// Number Functions
+var numDecimal = function () {
+
+
+};
+
+var numFuzzy = function () {
+
+
+};
+
+var numDates = function () {
+
+
+};
+
+var numNumber = function () {
+
+
+};
+
+// Array Functions
+var arrValue = function () {
+
+
+};
+
+var arrTotal = function () {
+
+
+};
+
+var arrSorted = function () {
+
+
+};
+
+
