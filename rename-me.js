338a900c6ae466bf30f44ebcb137995aa2cfb977
appstrategy <eprleads@gmail.com>
2013-05-01 12:46:23 -0400

    function 9 in number lib passing to main code, only for numbers need to fix for nan

--- rename-me.js
+++ rename-me.js
@@ -44,24 +44,22 @@ var stringLibrary = function () {
 
 
 		};
+
+		// Function 3
+		// Is the string a URL? (Does it start with http: or https:?).
+		var stringUrl = function (val) {
+				var url = val;
+				if (url.substring(0,7) == "http://" || url.substring(0,8) == "https://") {
+						return true;
+
+				}
+
+				
+
+
+		};
+
 /*
-// Function 3
-// Is the string a URL? (Does it start with http: or https:?).
-var stringUrl = function (checkUrl) {
-		var url = checkUrl
-		if () {
-
-			console.log();
-
-		} else {
-
-			console.log();
-
-		}
-
-
-};
-
 // Function 4
 // Title-case a string (split into words, then uppercase the first letter of each word).
 var stringTitle = function (stringTitle) {
@@ -78,7 +76,8 @@ var stringSeparator = function () {
 */
 			return {
 							"stringPhone": stringPhone,
-							"stringEmail": stringEmail
+							"stringEmail": stringEmail,
+							"stringUrl": stringUrl
 
 
 
@@ -86,17 +85,17 @@ var stringSeparator = function () {
 
 };
 
-/*
+
 // Number Library
 
 var numberLibrary = function () {
 
 			// Function 6
 			// Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10.
-			var numDecimal = function () {
+//			var numDecimal = function () {
 
 
-			};
+//			};
 /*
 // Function 7
 // Fuzzy-match a number: is the number above or below a number within a certain percent?
@@ -111,25 +110,32 @@ var numDates = function () {
 
 
 };
+*/
+		// Function 9
+		// Given a string version of a number such as "42", return the value as an actual Number, such as 42.
+		var parseString = function (val) {
+			pString = val;
+			return parseInt(pString);
 
-// Function 9
-// Given a string version of a number such as "42", return the value as an actual Number, such as 42.
-var numNumber = function () {
+
+			
 
 
-};
+		};
 
 			return {
-							"numDecimal": numDecimal
+						//	"numDecimal": numDecimal
+							"parseString": parseString
 
 			}
 
 };
 
-
+/*
 // Array Library
 
 var arrayLibrary = fucntion () {
+	
 /*
 // Function 10
 // Find the smallest value in an array that is greater than a given number.
@@ -155,10 +161,16 @@ var arrSorted = function () {
 }:
 */
 
+// String Library
 var sLib = stringLibrary();
 var phoneString = "123-4567";
 console.log("Dash is" + sLib.stringPhone(phoneString));
 var emailString = "test@test.com";
 console.log("email is" + sLib.stringEmail(emailString));
-//var nLib = numberLibrary();
+var url = "http//www.test.com";
+console.log("Url is " + sLib.stringUrl(url));
+// Number Libraray
+var nLib = numberLibrary();
+var parseTest = "34";
+console.log("Return Number" + nLib.parseString(parseTest));
 //var aLib = arrayLibrary();
